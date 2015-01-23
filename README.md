80fc37f597df73af06339534dd628ebad94ecfff
soh335 <sugarbabe335@gmail.com>
2015-01-23 21:58:55 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# nasne-conflict-pushbullet
+
+send push notification via [pushbullet](https://www.pushbullet.com/) if nasne recording schedule is conflicted
+
+## Install
+
+```
+$ go get github.com/soh335/nasne-conflict-pushbullet
+```
+
+
+## Usage
+
+```
+$ nasne-conflict-pushbullet --host x.x.x.x --apikey xxxxxxxxxxx
+```
+
+## License
+
+* MIT
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/soh335/nasne"
+	"github.com/soh335/nasne/xsrs"
+	"github.com/xconstruct/go-pushbullet"
+)
+
+var (
+	host   = flag.String("host", "", "host of nasne")
+	port   = flag.String("port", "64230", "port of nasne for request xsrs")
+	apikey = flag.String("apikey", "", "apikey of pushbullet")
+)
+
+func main() {
+	flag.Parse()
+
+	if err := _main(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func _main() error {
+	if *host == "" {
+		return fmt.Errorf("host is required")
+	}
+
+	if *port == "" {
+		return fmt.Errorf("port is required")
+	}
+
+	if *apikey == "" {
+		return fmt.Errorf("apikey is required")
+	}
+
+	root, err := nasne.GetRecordScheduleList(net.JoinHostPort(*host, *port))
+	if err != nil {
+		return err
+	}
+
+	for _, item := range root.Items {
+		if item.ConflictID != "0" {
+			if err := _notify(&item); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func _notify(i *xsrs.Item) error {
+	layout1 := "2006-01-02T15:04:05-0700"
+	layout2 := "2006-01-02 15:04:05"
+
+	start, err := time.Parse(layout1, i.ScheduledStartDateTime)
+	if err != nil {
+		return err
+	}
+	duration, err := strconv.Atoi(i.ScheduledDuration)
+	if err != nil {
+		return err
+	}
+	end := start.Add(time.Second * time.Duration(duration))
+
+	n := &pushbullet.Note{
+		Body:  fmt.Sprintf("%s (%s ~ %s) seems to be conflict", i.Title, start.Format(layout2), end.Format(layout2)),
+		Type:  "note",
+		Title: "confilict recording schedule",
+	}
+	return pushbullet.New(*apikey).Push(n)
+}
